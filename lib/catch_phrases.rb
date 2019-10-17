8a1455ed465f305bf03054a8ef9b59e5bacc9970
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 01:21:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,3 @@
-describe "mario" do
-  it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's-a me, Mario!"
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-  end
-end
-
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
-end
+def catch_phrase(phrase)
+  puts phrase	
+end	end
