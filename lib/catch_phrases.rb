de5fd111a557cea020db12664eaea4cd461f3da0
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 01:27:51 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,12 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
+def catch_phrase(phrase)
+  puts phrase	
+end	end
 
-def toadstool
-  puts status
-end
+require "spec_helper"
+
+
+describe "#catch_phrase" do 	
+  it "puts out a catch phrase" do 	  
+    phrase = "It's-a me, Mario!"	    
+    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout	    
+  end	  end
