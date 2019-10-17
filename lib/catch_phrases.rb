f23e8dcf38115d9efcf004fba19305f69b0bd68a
mengistiab woldearegay <solmengs@gmail.com>
2019-10-17 01:15:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,13 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+describe "mario" do
+  it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
+    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
 
-def toadstool
-  puts status
+describe "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
 end
